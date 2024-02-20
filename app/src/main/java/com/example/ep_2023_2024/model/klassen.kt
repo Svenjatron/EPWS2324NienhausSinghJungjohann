2700fd38a6da27e11948d4af2488cbe7354b393c
Caroline Jungjohann <caroline.jungjohann@smail.th-koeln.de>
2024-02-20 18:58:59 +0100

    Klassenstruktur angepasst

--- app/src/main/java/com/example/ep_2023_2024/model/klassen.kt
+++ app/src/main/java/com/example/ep_2023_2024/model/klassen.kt
@@ -1,5 +1,7 @@
 package com.example.ep_2023_2024.model
 
+import java.util.Scanner
+
 // Benutzer:innen Klassen //
 
 abstract class Benutzer(
@@ -34,8 +36,7 @@ class Schueler(
 
 // Schulklassen Klassen //
 
-abstract class Schulklasse
-
+abstract class Schulklasse(zahl: Int, buchstabe: Char)
 abstract class Chat
 
 class Schulgruppe(
@@ -66,25 +67,78 @@ class Privat_A(
     var bearbeitendeSchueler: List<Schueler> = emptyList()
 )
 
-class Teilaufgabe(
-    var title: String = "",
-    var description: String = "",
-    var question: String = "",
-    var media: Medien? = null,
-    var answerList: List<Antwort> = emptyList()
-)
-
-abstract class Medien
 
 data class Antwort(
-    var answerText: String = "",
-    var answerMedia: Medien? = null,
     var student: Schueler? = null,
     var subtask: Teilaufgabe? = null,
     var isCorrect: Boolean = false,
     var correctAnswer: String = "",
-    var isPrivate: Boolean = false
+    var isPrivate: Boolean = false,
+    val studentAnswer: List<Any>
 )
+
+open class Teilaufgabe(
+    var title: String = "",
+    var description: String = "",
+    var question: String = "",
+    var answerList: MutableList<Antwort> = mutableListOf<Antwort>(),
+    val context: String = "",
+    open val correctAnswer: List<Any> = emptyList()
+) {
+    open fun displayTask() {}
+    open fun answerTask(): MutableList<Int> {
+        val e = mutableListOf<Int>()
+        return e
+    }
+
+    open fun approveAnswer() {}
+    open fun manualApproveAnswers() {
+    }
+}
+open class Teilaufgabe_MC(
+    title: String = "",
+    description: String = "",
+    question: String = "",
+    answerList: MutableList<Antwort> = mutableListOf<Antwort>(),
+    context: String = "",
+    override val correctAnswer: MutableList<Int> = mutableListOf<Int>(),
+    val possibleAnswers: List<String>
+): Teilaufgabe(title, description, question, answerList, context, correctAnswer) {
+
+    override fun displayTask() {
+        /**
+         * Zeigt die zu bearbeitende Teilaufgabe an.
+         * */
+        var j = 1
+        println(this.description)
+        println(this.question)
+        for (i in this.possibleAnswers) {
+            println("$j. $i")
+            j++
+        }
+    }
+
+    override fun answerTask():MutableList<Int>{
+        /**
+         * Gibt eine Möglichkeit zur Bearbeitung der vorher angezeigten Aufgabe.
+         * */
+        val s = Scanner(System.`in`)
+        val answer_list = mutableListOf<Int>()
+        println("You can give multiple answers. If you're finished, enter 0. For more context, enter 9.")
+        println ("Please enter the number for your answer and hit enter. Then you can add more if you want.")
+        while (true){
+            val answer: Int = s.nextInt()
+            if (answer != 0 && answer != 9 ){
+                answer_list.add(answer)
+            }else if (answer == 9)  print(this.context)
+            else break
+        }
+        return answer_list
+    }
+}
+
+
+
 
 // Tag Klasse //
 
